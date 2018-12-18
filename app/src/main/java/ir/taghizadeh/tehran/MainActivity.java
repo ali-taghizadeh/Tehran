ac9db1d7807f2cb27cb71edb4eeb52e14291d908
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2018-12-18 11:41:17 +0330

    take authentication code out of Activity

--- app/src/main/java/ir/taghizadeh/tehran/MainActivity.java
+++ app/src/main/java/ir/taghizadeh/tehran/MainActivity.java
@@ -1,89 +1,45 @@
 package ir.taghizadeh.tehran;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.util.Arrays;
+import ir.taghizadeh.tehran.helpers.AuthenticationHelper;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private static final String ANONYMOUS = "anonymous";
     private static final int RC_SIGN_IN = 1;
+    AuthenticationHelper mAuthenticationHelper;
     private String mUsername;
-
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mUsername = ANONYMOUS;
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-                if (firebaseUser != null) {
-                    mUsername = firebaseUser.getDisplayName();
-                } else {
-                    mUsername = ANONYMOUS;
-                    startActivityForResult(
-                            AuthUI.getInstance()
-                                    .createSignInIntentBuilder()
-                                    .setIsSmartLockEnabled(false)
-                                    .setAvailableProviders(Arrays.asList(
-                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
-                                            new AuthUI.IdpConfig.EmailBuilder().build()))
-                                    .build(),
-                            RC_SIGN_IN);
-                }
-            }
-        };
+        mAuthenticationHelper = new AuthenticationHelper(this, RC_SIGN_IN);
+        mUsername = mAuthenticationHelper.getUsername();
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN){
-            if (resultCode == RESULT_OK)
-                Toast.makeText(this, "Signed in", Toast.LENGTH_SHORT).show();
-            else if (resultCode == RESULT_CANCELED){
+        if (requestCode == RC_SIGN_IN && resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Signed in canceled", Toast.LENGTH_SHORT).show();
                 finish();
-            }
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+        mAuthenticationHelper.addAuthStateListener();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (mAuthStateListener != null)
-            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        mAuthenticationHelper.removeAuthStateListener();
     }
 }
---
+++ app/src/main/java/ir/taghizadeh/tehran/helpers/AuthenticationHelper.java
@@ -0,0 +1,64 @@
+package ir.taghizadeh.tehran.helpers;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import java.util.Arrays;
+
+public class AuthenticationHelper {
+
+    private String mUsername;
+    private static final String ANONYMOUS = "anonymous";
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private Activity activity;
+    private int RC_SIGN_IN;
+
+
+    public AuthenticationHelper(Activity activity, int RC_SIGN_IN) {
+        this.activity = activity;
+        this.RC_SIGN_IN = RC_SIGN_IN;
+        mUsername = ANONYMOUS;
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        attachAuthListener();
+    }
+
+    private void attachAuthListener() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    mUsername = firebaseUser.getDisplayName();
+                } else {
+                    mUsername = ANONYMOUS;
+                    activity.startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    public String getUsername(){
+        return mUsername;
+    }
+
+    public void addAuthStateListener(){
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    public void removeAuthStateListener(){
+        if (mAuthStateListener != null)
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+}
