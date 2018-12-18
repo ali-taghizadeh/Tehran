bd1fd7018ce925b6ca9e69be1f1a142c4d5cf6ed
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2018-12-19 00:59:26 +0330

    add marker and start camera

--- app/src/main/java/ir/taghizadeh/tehran/helpers/MapHelper.java
+++ app/src/main/java/ir/taghizadeh/tehran/helpers/MapHelper.java
@@ -1,18 +1,25 @@
 package ir.taghizadeh.tehran.helpers;
 
-import android.app.Activity;
-import android.content.Context;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import ir.taghizadeh.tehran.R;
 
 public class MapHelper implements OnMapReadyCallback {
+
+    static final LatLng DOWNTOWN = new LatLng(35.700969, 51.391188);// Our entry point is always downtown
+    private GoogleMap googleMap;
+    private BitmapDescriptor centerMarkerIcon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA);
 
     public MapHelper(FragmentActivity fragmentActivity) {
         SupportMapFragment mapFragment = (SupportMapFragment) fragmentActivity.getSupportFragmentManager().findFragmentById(R.id.map);
@@ -23,6 +30,30 @@ public class MapHelper implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        this.googleMap = googleMap;
+        addMarker(DOWNTOWN, "Downtown", "We always start from here", centerMarkerIcon);
+        startCamera(DOWNTOWN);
+    }
+
+    public void addMarker(LatLng position, String title, String snippet, BitmapDescriptor bitmapDescriptor) {
+        if (googleMap != null) {
+            Marker marker = googleMap.addMarker(new MarkerOptions()
+                    .position(position)
+                    .title(title)
+                    .snippet(snippet)
+                    .icon(bitmapDescriptor));
+            marker.showInfoWindow();
+        }
+    }
+
+    public void startCamera(LatLng position) {
+        //Build camera position
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(position)
+                .zoom(17).build();
+        //Zoom in and animate the camera.
+        if (googleMap != null) {
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
     }
 }
