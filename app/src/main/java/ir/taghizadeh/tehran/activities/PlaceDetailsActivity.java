4dce377acb27a93b4e7392c25e8798d59943107b
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2019-01-06 11:53:03 +0330

    add documentation to PlaceDetailsActivity

--- app/src/main/java/ir/taghizadeh/tehran/activities/PlaceDetailsActivity.java
+++ app/src/main/java/ir/taghizadeh/tehran/activities/PlaceDetailsActivity.java
@@ -29,6 +29,23 @@ import ir.taghizadeh.tehran.helpers.Constants;
 import ir.taghizadeh.tehran.models.Comments;
 import ir.taghizadeh.tehran.models.NewPlace;
 
+/**
+ * <h1>PlaceDetailsActivity</h1>
+ *
+ * When user taps on an item from list of places, this activity pops up.
+ * It could have come here with just a key and use that key to execute a query and
+ * populate the page by fresh details. But I just pass the details through an intent
+ * and use them statically.
+ *
+ * In this activity, we need all dependencies except GeoFire,
+ * as a result, when we look at the order of moduleActivities, DatabaseModuleActivity would be
+ * the right one to extend this class with.
+ *
+ * @author Ali Taghizadeh Gevari
+ * @version 1.0
+ * @since 2019-01-06
+ */
+
 public class PlaceDetailsActivity extends DatabaseModuleActivity {
 
     @BindView(R.id.text_place_details_title)
@@ -78,14 +95,29 @@ public class PlaceDetailsActivity extends DatabaseModuleActivity {
         setupUI();
     }
 
+    /**
+     * Any disposable get disposed when this method is called
+     */
     @Override
     protected void onPause() {
         super.onPause();
-        dispose();
+        disposeGeneral();
     }
     // endregion
 
     // region MANAGE UI
+
+    /**
+     * It opens the bundle here and populates the page.
+     * Among these data we have the key of this node in Firebase database which we
+     * use it to execute a query to get the right comments about this place.
+     * To sum it up, there are 3 nodes in Firebase database :
+     * 1 - PLACES : where we store details of a place (title, description, likes, dislikes ...)
+     * 2 - PLACES_LOCATION : where we store the location of the place.
+     * 3 - PLACES_COMMENTS : where we store the comments about the place
+     * Talking about a particular place, it will have a unique key to store data in each node,
+     * so it helps executing queries.
+     */
     private void setupUI() {
         setFullScreen();
         initializeList();
@@ -110,13 +142,19 @@ public class PlaceDetailsActivity extends DatabaseModuleActivity {
     // endregion
 
     // region QUERY ON COMMENTS
+
+    /**
+     * When this method is called, it uses the key to execute a query on comments node.
+     * it waits for 2 secs and then it updates the list of comments and when its task is
+     * done, it disposes all disposables.
+     */
     private void attachComments() {
         query(Constants.PLACES_COMMENTS, mKey);
         Observable.interval(1, TimeUnit.SECONDS)
                 .take(2)
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnSubscribe(disposable -> {
-                    getCompositeDisposable().add(disposable);
+                    getGeneralDisposable().add(disposable);
                     progress_place_details.setVisibility(View.VISIBLE);
                     recyclerView_place_details.setVisibility(View.GONE);
                     image_place_details_empty_list.setVisibility(View.GONE);
@@ -133,7 +171,7 @@ public class PlaceDetailsActivity extends DatabaseModuleActivity {
                     }else {
                         updateList();
                     }
-                    dispose();
+                    disposeGeneral();
                 })
                 .subscribe();
     }
@@ -151,6 +189,12 @@ public class PlaceDetailsActivity extends DatabaseModuleActivity {
     // endregion
 
     // region HANDLE CLICK EVENTS
+
+    /**
+     * When send button is clicked, first it tries to validate the textInput, if it was ok,
+     * it creates a new object of comments and pushes it up with the right key to store in database.
+     * Then it calls attachComments() which it ends up in executing a query again to get the most fresh data.
+     */
     @OnClick(R.id.image_place_details_send)
     void addComment() {
         if (isInputValid(Objects.requireNonNull(edittext_place_details_comment.getText()).toString(), edittext_place_details_comment, "Write your comment first")){
@@ -159,6 +203,14 @@ public class PlaceDetailsActivity extends DatabaseModuleActivity {
             attachComments();
         }
     }
+
+    /**
+     * The point here about like/dislike button is that a user can like/dislike a place
+     * multiple times by navigating back and forward which is an issue.
+     * There are some solutions to this issue for sure, like making a new node dedicated to likes/dislikes,
+     * or binding the user to likes/dislikes to have more control over them.
+     *
+     */
 
     @OnClick(R.id.image_place_details_like)
     void like(){
@@ -181,13 +233,13 @@ public class PlaceDetailsActivity extends DatabaseModuleActivity {
     // endregion
 
     // region CREATE AND CLEAR DISPOSABLES
-    private CompositeDisposable getCompositeDisposable() {
+    private CompositeDisposable getGeneralDisposable() {
         if (compositeDisposable == null || compositeDisposable.isDisposed())
             compositeDisposable = new CompositeDisposable();
         return compositeDisposable;
     }
 
-    private void dispose() {
+    private void disposeGeneral() {
         if (compositeDisposable != null && !compositeDisposable.isDisposed())
             compositeDisposable.clear();
     }
